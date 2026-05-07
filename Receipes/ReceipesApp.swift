2385dd660b938ba288dae1bc8e31290758532d13
Amosse Edouard <amosse.edouard@instant-system.com>
2026-05-07 20:07:28 +0200

    Initial Commit

---
+++ Receipes/ReceipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceipesApp.swift
+//  Receipes
+//
+//  Created by Amosse Edouard on 07/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct ReceipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
